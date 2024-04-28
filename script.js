9aa74e8087b6dd32ddf665e009a8f205e5b3ed8b
VEDANT SOMVANSHI <94733942+vedant13012002@users.noreply.github.com>
2024-04-29 01:43:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const first = document.getElementsByName("fname")[0].value;
+	const last = document.getElementsByName("lname")[0].value;
+
+	const fullName = first.trim()+" "+last.trim;
+	alert(fullName);
+	
 
 }
